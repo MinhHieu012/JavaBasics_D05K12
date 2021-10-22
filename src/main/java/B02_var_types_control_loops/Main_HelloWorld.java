278516e9640faf2c5f740aa7feedee99d91a6d54
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-10-22 16:31:26 +0700

    Initial commit

---
+++ src/main/java/B02_var_types_control_loops/Main_HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package B02_var_types_control_loops;
+
+/**
+ *
+ * @author huynq
+ */
+public class Main_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello Btec-D05-K12");
+    }
+
+}
