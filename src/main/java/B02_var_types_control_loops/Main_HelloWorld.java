aee834d0b0d06f17b7de7466530d10e16cb3a5e6
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-10-25 15:02:07 +0700

    java methods

--- src/main/java/B02_var_types_control_loops/Main_HelloWorld.java
+++ src/main/java/B02_var_types_control_loops/Main_HelloWorld.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package B02_var_types_control_loops;
 
 /**
@@ -17,6 +16,36 @@ public class Main_HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello Btec-D05-K12");
+
+        int s = sum(2, 3);
+        System.out.println(s);
+
+        System.out.println(sum(4, 6));
+
+        System.out.println(sum(3.5, 2.5));
+
+        System.out.println(sum(3));
     }
 
+    private static int sum(int a, int b) {
+
+        System.out.println("int version");
+        return a + b;
+    }
+
+    // overloading method
+    private static double sum(double a, double b) {
+
+        System.out.println("double version");
+        return a + b;
+    }
+
+    // recursion (1 + 2 + ... + k)
+    public static int sum(int k) {
+        if (k > 0) {
+            return k + sum(k - 1);
+        } else {
+            return 0;
+        }
+    }
 }
